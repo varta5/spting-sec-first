947d76a901caa849f15eefec393fe59f0cf42560
varta5 <varga.tamas.pro@gmail.com>
2022-06-13 20:55:27 +0200

    initiate Spring Security Application

---
+++ src/main/java/com/gfa/springsecurityfirstpractice/SpringSecurityFirstPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.gfa.springsecurityfirstpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFirstPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityFirstPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gfa/springsecurityfirstpractice/SpringSecurityFirstPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfa.springsecurityfirstpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFirstPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
