a1a84f50f94c20c608ddb00ede1a5c9d7d57eca8
varta5 <varga.tamas.pro@gmail.com>
2022-06-15 15:34:27 +0200

    initiate Spring Security app with JWT authentication

---
+++ spring-security-jwt/src/main/java/com/gfa/springsecurityjwt/controllers/HomeController.java
@@ -0,0 +1,54 @@
+package com.gfa.springsecurityjwt.controllers;
+
+import com.gfa.springsecurityjwt.models.AuthenticationRequest;
+import com.gfa.springsecurityjwt.models.AuthenticationResponse;
+import com.gfa.springsecurityjwt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public HomeController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String showHome() {
+        return "<h1>Welcome</h1>";
+    }
+
+    @RequestMapping(path = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException exception) {
+            throw new Exception("Incorrect username or password", exception);
+        }
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
---
+++ spring-security-jwt/src/main/java/com/gfa/springsecurityjwt/services/MyUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.gfa.springsecurityjwt.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("theuser", "password", List.of());
+    }
+
+}
