48436f7dc3631673b7c53d5f7744d96db8aab8bf
varta5 <varga.tamas.pro@gmail.com>
2022-06-17 15:57:40 +0200

    add method with conversions of fields using ModelMapper

--- modelmapper-first/src/main/java/PracticeModelMapper.java
+++ modelmapper-first/src/main/java/PracticeModelMapper.java
@@ -1,24 +1,23 @@
 import models.Example;
 import models.ExampleFieldNamesMismatching;
+import models.ExampleWithConversion;
 import models.ExampleWithLimitedMatchingFields;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.modelmapper.TypeMap;
-import org.modelmapper.internal.MappingEngineImpl;
-import org.modelmapper.spi.Mapping;
-import org.modelmapper.spi.MappingEngine;
 
 import java.util.List;
 
 public class PracticeModelMapper {
 
     public static void main(String[] args) {
-        Example example = new Example(1, 123, "This is example test", true,
-                List.of("One text", "Another text", "Third and last text"));
+        Example example = new Example(1, 123, "g", true,
+                List.of("One text", "Another text", "Third", "Fourth 4 yeah", "", "Last one if the sixth!"));
         // ExampleWithLimitedMatchingFields exampleWithLimitedMatchingFields = convertExampleToLimitedFields(example);
-        // ModelMapper modelMapper = new ModelMapper();
-        // TypeMap<Example, ExampleFieldNamesMismatching> typeMap = modelMapper.createTypeMap(Example.class, ExampleFieldNamesMismatching.class);
-        ExampleFieldNamesMismatching exampleFieldNamesMismatching = convertExampleToFieldNamesMismatching(example);
+        // ExampleFieldNamesMismatching exampleFieldNamesMismatching = convertExampleToFieldNamesMismatching(example);
+        // ExampleWithConversion exampleWithConversion = modelMapper.map(example, ExampleWithConversion.class);
+        // ExampleWithConversion exampleWithConversion = convertFields(example);
 
         System.out.println("Done.");
     }
@@ -36,5 +35,38 @@ public class PracticeModelMapper {
         typeMap.addMapping(Example::isAwesome, ExampleFieldNamesMismatching::setThisIsABoolean);
         typeMap.addMapping(Example::getListOfWords, ExampleFieldNamesMismatching::setThisIsAListOfStrings);
         return modelMapper.map(example, ExampleFieldNamesMismatching.class);
+    }
+
+    public static ExampleWithConversion convertFields(Example example) {
+        ModelMapper modelMapper = new ModelMapper();
+        TypeMap<Example, ExampleWithConversion> typeMap = modelMapper.createTypeMap(Example.class, ExampleWithConversion.class);
+        Converter<Integer, Integer> doublingNumber = new AbstractConverter<Integer, Integer>() {
+            @Override
+            protected Integer convert(Integer source) {
+                return source == null ? null : source * 2;
+            }
+        };
+        typeMap.addMappings(src -> src.using(doublingNumber).map(Example::getaNumber, ExampleWithConversion::setaNumber));
+        Converter<String, Character> getLastCharacterOfText = new AbstractConverter<String, Character>() {
+            @Override
+            protected Character convert(String source) {
+                if (source == null || source.isEmpty()) {
+                    return null;
+                }
+                return source.charAt(source.length() - 1);
+            }
+        };
+        typeMap.addMappings(src -> src.using(getLastCharacterOfText).map(Example::getText, ExampleWithConversion::setLastCharacterOfText));
+        Converter<List<String>, Integer> countOfStrings = new AbstractConverter<List<String>, Integer>() {
+            @Override
+            protected Integer convert(List<String> source) {
+                if (source == null) {
+                    return 0;
+                }
+                return source.size();
+            }
+        };
+        typeMap.addMappings(src -> src.using(countOfStrings).map(Example::getListOfWords, ExampleWithConversion::setNumberOfStrings));
+        return modelMapper.map(example, ExampleWithConversion.class);
     }
 }
---
+++ modelmapper-first/src/main/java/models/ExampleWithConversion.java
@@ -0,0 +1,27 @@
+package models;
+
+public class ExampleWithConversion {
+    private long id;
+    private int aNumber;
+    private char lastCharacterOfText;
+    private int numberOfStrings;
+
+    public ExampleWithConversion() {
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setaNumber(int aNumber) {
+        this.aNumber = aNumber;
+    }
+
+    public void setLastCharacterOfText(char lastCharacterOfText) {
+        this.lastCharacterOfText = lastCharacterOfText;
+    }
+
+    public void setNumberOfStrings(int numberOfStrings) {
+        this.numberOfStrings = numberOfStrings;
+    }
+}
